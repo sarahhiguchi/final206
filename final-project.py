0c6b180d5fddbc0619ef62d2367155ecee8741e9
Sarah Higuchi-Crowell <sarahhiguchi-crowell@0587385495.wireless.umich.net>
2019-12-11 14:17:29 -0500

    wrote one function for location search with songkick api

--- final-project.py
+++ final-project.py
@@ -1,5 +1,24 @@
 import requests
+import spotipy
+import sqlite3
+import json
 
 
-spotify_clientID="c2120610f7f4473781820928004f3760" 
-response = requests.get("https://api.spotify.com/v1/artists/{id}/top-tracks")
+def get_locid_songkick(loc):
+    try: 
+        api_key = "kmRJqsmZhyq4LTS5"
+        loc_query = loc + "&apikey=" + api_key
+        loc_url = "https://api.songkick.com/api/3.0/search/locations.json?query=" + loc_query
+        loc_r = requests.get(loc_url)
+        info = json.loads(loc_r.text)
+        return info["resultsPage"]["results"]["location"][0]["metroArea"]["id"]
+    except: 
+        print("Error when reading from url")
+        info = {}
+    return info
+
+def get_data_songkick
+
+print(get_locid_songkick("London"))
+
+
