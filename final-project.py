c4f712f0d282b306c4d6bf3a85e27796d505d130
Sarah Higuchi-Crowell <sarahhiguchi-crowell@Sarahs-MacBook-Air.local>
2019-12-17 19:49:42 -0500

    changed font size for pie chart

--- final-project.py
+++ final-project.py
@@ -274,7 +274,7 @@ def make_pie(percentages):
     sizes = list(percentages.values())
     fig1, ax1 = plt.subplots()
     ax1.pie(sizes, labels=labels, autopct='%1.1f%%',
-            shadow=True, startangle=90)
+            shadow=True, startangle=90, textprops={'fontsize': 6})
     ax1.axis('equal')  # Equal aspect ratio ensures that pie is drawn as a circle.
 
     plt.show()
